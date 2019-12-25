7c3c59a94c868880f66e0a28ca720b7fd5cb9f19
soriaj <javier.soria@gmail.com>
2019-12-25 05:24:29 -0800

    - Update App to Class

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <main className='App'>
-      <h1>Test From App</h1>
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main className='App'>
+        <h1>Test From App</h1>
+      </main>
+    );
+  }
 }
 
 export default App;
