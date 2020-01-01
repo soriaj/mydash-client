3afb29ff7275814f50d04d444d7cc9ada50b462c
soriaj <javier.soria@gmail.com>
2020-01-01 06:07:55 -0800

    - Create Event Date, Timeline Card components - Update mock events data - Update DashboardEvents with timeline generation

--- src/components/EventsTimeLine/EventsTimeLine.js
+++ src/components/EventsTimeLine/EventsTimeLine.js
@@ -1,76 +1,23 @@
 import React, { Component } from 'react';
 import TravelerContext from '../../context/TravlerContext'
+import EventDates from '../EventDates/EventDates'
 
 class EventsTimeLine extends Component {
     static contextType = TravelerContext
+
     render() {
+        const { event } = this.props
+        console.log(event)
         return (
             <div className='timeline'>
-                <div className='timeline-month'><strong>{'Month'}</strong></div>
-                <div className='timeline-date arrow'>
-                    {'12/30/19'}
-                </div>
-                <div className='timeline-section'>
-                    <div className='timeline-card'>
-                        <div className='card-title'>
-                            {'Event Title'}
-                        </div>
-                        <div className='card-details'>
-                            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, 
-                                vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim 
-                                per, habeo iusto primis ea eam.
-                            </p>
-                        </div>
-                    </div>
-                    <div className='timeline-card'>
-                        <div className='card-title'>
-                            {'Event Title'}
-                        </div>
-                        <div className='card-details'>
-                            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, 
-                                vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim 
-                                per, habeo iusto primis ea eam.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='timeline-date arrow'>
-                    {'12/29/19'}
-                </div>
-                <div className='timeline-section'>
-                    <div className='timeline-card'>
-                        <div className='card-title'>
-                            {'Event Title'}
-                        </div>
-                        <div className='card-details'>
-                            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, 
-                                vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim 
-                                per, habeo iusto primis ea eam.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                {/* <!-- TIMELINE NEW MONTH--> */}
-                <div className='timeline-month'><strong>{'Month'}</strong></div>
-                <div className='timeline-date arrow'>
-                    {'11/30/19'}
-                </div>
-                <div className='timeline-section'>
-                    <div className='timeline-card'>
-                        <div className='card-title'>
-                            {'Event Title'}
-                        </div>
-                        <div className='card-details'>
-                            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, 
-                                vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim 
-                                per, habeo iusto primis ea eam.
-                            </p>
-                        </div>
-                    </div>
-                {/* <!-- TIMELINE END--> */}
-                </div>
+                <div className='timeline-month'><strong>{event.month}</strong></div>
+                {event.event.map((events, idx) => (
+                    <EventDates 
+                        key={idx}
+                        dates={events.event_date}
+                        event_detail={events.event_details}
+                    />
+                ))}
             </div>
         );
     }
