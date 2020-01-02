4d45deefb16fab1bc1e97cca5bf6075f8e69ab47
soriaj <javier.soria@gmail.com>
2020-01-02 12:50:46 -0800

    - Update Login and Sign up link color - Update SideNavEvents to with correct data and links to events

--- src/components/SideNavEventsItems/SideNavEventsItems.js
+++ src/components/SideNavEventsItems/SideNavEventsItems.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
 import { FaCalendarDay } from 'react-icons/fa'
+import { NavLink } from 'react-router-dom'
 
 export default class SideNavEventsItems extends Component {
     render() {
-        const { event } = this.props
-        // const name = event.event_details.map(cur => cur.name)
-        console.log(event.event)
-        // event.map(cur => console.log(cur.name))
+        const { event_id, name, day} = this.props
         return (
             <div className='list-item'>
                 <span><FaCalendarDay className='fas fa-calendar-day'></FaCalendarDay></span>
-                <span className='list-item-title'>{'name'}</span>
+                <span className='list-item-title'><NavLink to={`/dashboard/events/${day}/${event_id}`}>{name}</NavLink></span>
             </div>
         )
     }
