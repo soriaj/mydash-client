233433dc6bd9cacb72193809b113335222834132
soriaj <javier.soria@gmail.com>
2020-03-02 04:51:16 -0800

    - Update AddEventForm date in dashboad display - Update event timeline buttons
    Former-commit-id: 23fae19ab4b6cd912d208f00623ebfa99b2c6a5b

--- src/components/AddEventForm/AddEventForm.js
+++ src/components/AddEventForm/AddEventForm.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
-import { FaCalendarPlus, FaLocationArrow, FaEdit, FaSave, FaRegSave } from 'react-icons/fa'
+import { FaCalendarPlus, FaLocationArrow, FaEdit, FaSave, FaRegSave, FaClock } from 'react-icons/fa'
 import TravelerContext from '../../context/TravlerContext'
 import Loading from '../Loading/Loading'
 import config from '../../config'
 import BackToDashboard from '../BackToDashboard/BackToDashboard'
 import './AddEventForm.css'
 import SaveButton from '../SaveButton/SaveButton'
+import DatePicker from 'react-datepicker'
+// import "react-datepicker/dist/react-datepicker.css";
+
 
 const uuidv4 = require('uuid/v4')
 
 export default class NewListForm extends Component {
     state = {
         error: null,
-        loading: false
+        loading: false,
+        startDate: new Date()
     }
     static contextType = TravelerContext
 
@@ -32,19 +36,12 @@ export default class NewListForm extends Component {
     }
     onSubmit = ev => {
         ev.preventDefault()
-        const { event_name, event_loc, description } = ev.target
+        const { event_name, event_date, event_loc, description } = ev.target
+        const { startDate } = this.state
         const { addEventItem } = this.context
-        const day = new Intl.DateTimeFormat("en-US", {
-            year: "numeric",
-            month: "long",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit"
-          }).format(new Date())
         const newEvent = {
             id: uuidv4(),
-            date: day,
+            date: startDate,
             event_name: event_name.value,
             event_loc: event_loc.value,
             description: description.value
@@ -61,6 +58,11 @@ export default class NewListForm extends Component {
     backToDashboard = () => {
         this.props.history.push('/dashboard')
     }
+
+    handleDateChange = date => {
+        this.setState({ startDate: date })
+    }
+
     render() {
         const { error, loading } = this.state
         return (
@@ -83,8 +85,23 @@ export default class NewListForm extends Component {
                                     id="event_name" 
                                     placeholder='Enter Event Name' 
                                     className='input-field'
-                                    // required
+                                    required
                                     />
+                                <span className="focus-input-field"></span>
+                            </div>
+
+                            <div className='input-wrapper'>
+                                <FaClock className="fa-user icon"></FaClock>
+                                <label htmlFor="event_date" className='no-view'>Date</label>
+                                {/* <input 
+                                    type="text" 
+                                    name="event_date" 
+                                    id="event_date" 
+                                    placeholder='MM-DD-YYYY' 
+                                    className='input-field'
+                                    required
+                                    /> */}
+                                    <DatePicker selected={this.state.startDate} onChange={this.handleDateChange} />
                                 <span className="focus-input-field"></span>
                             </div>
 
