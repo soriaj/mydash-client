c9c9c71098fdbebbdf2df16bcece2c55cfb44251
Javier Soria <javier.soria@gmail.com>
2020-09-20 05:47:32 -0700

    - Update Landing page with basic app details - Update Landing page styling

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 import './Landing.css'
 
 export default class Landing extends Component {
+    goToSignUpPage = () => {
+        this.props.history.push('/signup')
+    }
     render() {
         return (
         <>
@@ -12,14 +15,26 @@ export default class Landing extends Component {
                     <header>
                         <h2>MyDash</h2>
                     </header>
-                    <p>Get ready to see the world. For the traveling expert or the on a whim vacationer. Plan your trip with Traveler.</p>
-                    <p>Save and view your flight information, local events, great food and more. An amazing adventure awaits.</p>
-                    <Link to={'/signup/'} className='btn-one'>Sign Up</Link>
+                    <p>An application to keep you productive and on track.  Allowing you to focus on three key areas.</p>
+                    <div>
+                        <h3>Todos</h3>
+                        <p>Create Todo lists, so you don't miss what needs to get completed.</p>
+                    </div>
+                    <div>
+                        <h3>Events</h3>
+                        <p>Keep track of important events with ease.</p>
+                    </div>
+                    <div className='last-landing-title'>
+                        <h3>Finanace</h3>
+                        <p>Keep an eye on where your money is going by tracking income and purchases.</p>
+                    </div>
+                    <div className="login-btn-container login-btn-landing">
+                        <button className="login-btn" onClick={this.goToSignUpPage}>Sign Up</button>
+                    </div>
+                    <div className='login-signup'>
+                        <p>Already have an account?<span><Link to='/login'>Login</Link></span></p>
+                    </div>
                 </div>
-            </div>
-
-            <div className='section-two'> 
-
             </div>
         </section>
         </>
