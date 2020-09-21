060585f12fd8ca5f6fcd18ca1ca9d9228164eb95
Javier Soria <javier.soria@gmail.com>
2020-09-21 14:59:15 -0700

    - Update project scrips for testing, build, deploy - Update tests to use react-router-dom

--- src/components/Landing/Landing.test.js
+++ src/components/Landing/Landing.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Landing from './Landing';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Landing />, div);
+    ReactDOM.render(<BrowserRouter><Landing /></BrowserRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/SideNav/SideNav.test.js
+++ src/components/SideNav/SideNav.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import SideNav from './SideNav';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<SideNav />, div);
+    ReactDOM.render(<BrowserRouter><SideNav /></BrowserRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
